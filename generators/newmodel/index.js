64d51736097295ff5a39d8b7ebf17a05fc74216c
Marius Obert <Obatzer@gmail.com>
2021-04-19 17:55:40 +0200

    Revert "Composeability"
    This reverts commit 9f8dea4242bf0245cbce4c754a2f498e039e58b4.

--- generators/newmodel/index.js
+++ generators/newmodel/index.js
@@ -12,7 +12,7 @@ module.exports = class extends Generator {
                 name: "modulename",
                 message: "To which module do you want to add a model?",
                 choices: modules || [],
-                when: modules ? modules.length > 1 : false
+                when: modules && modules.length > 1
             },
             {
                 type: "input",
@@ -65,7 +65,7 @@ module.exports = class extends Generator {
             this.options.oneTimeConfig.modelName = answers.modelName;
             this.options.oneTimeConfig.modelType = answers.modelType;
             this.options.oneTimeConfig.bindingMode = answers.bindingMode;
-            this.options.oneTimeConfig.modulename = answers.modulename || modules ? modules[0] : this.options.modulename;
+            this.options.oneTimeConfig.modulename = answers.modulename || modules[0];
 
             if (answers.modelType.includes("OData")) {
                 this.options.oneTimeConfig.url = answers.url;
