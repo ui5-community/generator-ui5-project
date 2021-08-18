2e367ed346aa6042dc5a4e98f9eba93c4a8d44d6
Georg Bischoff <georg.bischoff@sap.com>
2021-08-18 17:57:09 +0200

    Fixed es-lint issues in templates

--- generators/qunit/templates/test/testsuite.qunit.js
+++ generators/qunit/templates/test/testsuite.qunit.js
@@ -3,9 +3,10 @@ window.suite = function () {
 
     var oSuite = new parent.jsUnitTestSuite(),
         sContextPath = location.pathname.substring(0, location.pathname.lastIndexOf("/") + 1);
-        
+
     oSuite.addTestPage(sContextPath + "unit/unitTests.qunit.html");
     <% if (typeof(opa5Journeys) !== "undefined") { %>oSuite.addTestPage(sContextPath + "integration/opaTests.qunit.html");
-    <% } %>
+<% } -%>
+
     return oSuite;
 };
