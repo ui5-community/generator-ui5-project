496f8e223f61e521bade40c6bbf118157247475b
Volker Buzek <volker.buzek@js-soft.com>
2021-11-09 10:20:32 +0100

    revert: restore all template files
    ...from commit 3f3bbe96fa1e0fe0f289a68460f7e2ba5969c5ae
    as they are needed for (legacy) generation of apps with
    HTML-, JSON- and JS-files

---
+++ generators/newwebapp/templates/uimodule/webapp/model/models.js
@@ -0,0 +1,15 @@
+sap.ui.define(["sap/ui/model/json/JSONModel", "sap/ui/Device"],
+    /**
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel 
+     * @param {typeof sap.ui.Device} Device 
+     */
+    function (JSONModel, Device) {
+        "use strict";
+        return {
+            createDeviceModel: function () {
+                var oModel = new JSONModel(Device);
+                oModel.setDefaultBindingMode("OneWay");
+                return oModel;
+            }
+        };
+    });
