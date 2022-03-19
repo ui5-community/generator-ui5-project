c2729220a125ed4ae074c697d4544e57e827a96c
marianfoo <13335743+marianfoo@users.noreply.github.com>
2022-03-19 20:34:49 +0100

    fix: add plugin-search-dir to lint as well

--- generators/app/index.js
+++ generators/app/index.js
@@ -158,7 +158,7 @@ module.exports = class extends Generator {
                 "karma-ci": "karma start karma-ci.conf.js",
                 clearCoverage: "shx rm -rf coverage",
                 karma: "run-s clearCoverage karma-ci",
-                lint: "eslint ./**/webapp/**/*.js && prettier --check ./**/webapp/**/*.{js,xml}",
+                lint: "eslint ./**/webapp/**/*.js && prettier --plugin-search-dir=. --check ./**/webapp/**/*.{js,xml}",
                 "lint-fix": "eslint ./**/webapp/**/*.js --fix && prettier --plugin-search-dir=. --write ./**/webapp/**/*.{js,xml} --no-error-on-unmatched-pattern"
             },
             devDependencies: {
