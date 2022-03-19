fc34bacb77f9e7b59bba00c45fe0ec6779c08885
marianfoo <13335743+marianfoo@users.noreply.github.com>
2022-03-19 20:14:33 +0100

    Revert "fix(prettier): ignore unknown files in prettier"
    This reverts commit 3f2aa15ac4b8fa5604cd79aef1b401981dbc173e.

--- generators/app/index.js
+++ generators/app/index.js
@@ -159,8 +159,7 @@ module.exports = class extends Generator {
                 clearCoverage: "shx rm -rf coverage",
                 karma: "run-s clearCoverage karma-ci",
                 lint: "eslint ./**/webapp/**/*.js && prettier --check ./**/webapp/**/*.{js,xml}",
-                "lint-fix":
-                    "eslint ./**/webapp/**/*.js --fix && prettier --write ./**/webapp/**/*.{js,xml} --ignore-unknown"
+                "lint-fix": "eslint ./**/webapp/**/*.js --fix && prettier --write ./**/webapp/**/*.{js,xml} --no-error-on-unmatched-pattern"
             },
             devDependencies: {
                 shx: "^0.3.3",
