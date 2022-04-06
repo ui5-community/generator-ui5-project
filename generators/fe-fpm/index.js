d00a93125ab2342e560d28ad82cd0eff81d6bcf1
Tobias Queck <tobias.queck@sap.com>
2022-04-06 16:57:16 +0200

    add service generation

--- generators/fe-fpm/index.js
+++ generators/fe-fpm/index.js
@@ -1,12 +1,14 @@
 const Generator = require("yeoman-generator");
 const ui5Writer = require("@sap-ux/ui5-application-writer");
 const fpmWriter = require("@sap-ux/fe-fpm-writer");
+const serviceWriter = require("@sap-ux/odata-service-writer");
+const axios = require("@sap-ux/axios-extension");
 
 module.exports = class extends Generator {
     static displayName = "Create a new Fiori element flexible program model project";
 
     async prompting() {
-        this.answers =  await this.prompt([
+        this.answers = await this.prompt([
             {
                 type: "input",
                 name: "projectname",
@@ -42,7 +44,52 @@ module.exports = class extends Generator {
                     return "Please use alpha numeric characters only for the view name.";
                 },
                 default: 'Main'
+            }, {
+                type: 'input',
+                name: 'url',
+                message: 'Service url',
+                default: 'https://sapes5.sapdevcenter.com/sap/opu/odata/sap/SEPMRA_PROD_MAN',
+                validate: (s) => !!s
             }]);
+        
+        const url = new URL(this.answers.url);
+        this.answers.host = url.origin;
+        this.answers.path = url.pathname;
+        const service = axios.createServiceForUrl(this.answers.url, {
+            ignoreCertErrors: true
+        });
+
+        while (!this.answers.metadata) {
+            try {
+                this.answers.metadata = await service.metadata();
+            } catch (error) {
+                if (service.defaults?.auth?.username) {
+                    generator.log.error(error.cause.statusText);
+                }
+                if (error.cause.status === 401) {
+                    const { username, password } = await this.prompt([
+                        {
+                            type: 'input',
+                            name: 'username',
+                            message: 'Username',
+                            validate: (answer) => !!answer
+                        },
+                        {
+                            type: 'password',
+                            name: 'password',
+                            message: 'Password',
+                            validate: (answer) => !!answer
+                        }
+                    ]);
+                    service.defaults.auth = {
+                        username,
+                        password
+                    };
+                } else {
+                    throw error;
+                }
+            }
+        }
     }
 
     async writing() {
@@ -58,7 +105,12 @@ module.exports = class extends Generator {
         fpmWriter.enableFPM(this.destinationRoot(), {
             replaceAppComponent: true
         }, this.fs);
-
+        serviceWriter.generate(this.destinationRoot(), {
+            url: this.answers.host,
+            path: this.answers.path,
+            version: serviceWriter.OdataVersion.v4,
+            metadata: this.answers.metadata
+        }, this.fs);
         fpmWriter.generateCustomPage(
             this.destinationRoot(),
             {
