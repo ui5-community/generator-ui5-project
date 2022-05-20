9cecb12d8ecac7e3ef805a3bb1fe262605e3f154
Tobias Queck <tobias.queck@sap.com>
2022-05-20 13:06:58 +0200

    latest version of fiori-elements-writer

--- generators/newwebapp/index.js
+++ generators/newwebapp/index.js
@@ -1,6 +1,5 @@
 const Generator = require("yeoman-generator"),
     fileaccess = require("../../helpers/fileaccess"),
-    fs = require("fs").promises,
     path = require("path"),
     glob = require("glob"),
     chalk = require("chalk");
@@ -181,13 +180,14 @@ module.exports = class extends Generator {
                 },
                 appOptions: {
                     loadReuseLibs: platformIsLaunchpad
-                }
+                },
+                ui5:{
+                    
+                 }
             };
 
             try {
-                const _fs = await generateFreestyleTemplate(this.destinationPath(sModuleName), FreestyleApp);
-                await _fs.commit();
-
+                await generateFreestyleTemplate(this.destinationPath(sModuleName), FreestyleApp, this.fs);
                 // clean up @sap-ux/fiori-freestyle-writer artefacts not needed in easy-ui5
                 [
                     "ui5-local.yaml",
@@ -196,11 +196,22 @@ module.exports = class extends Generator {
                     ".npmignore" /* irrelevant */
                 ].map(async (file) => {
                     try {
-                        await fs.unlink(this.destinationPath(sModuleName, file));
+                        this.fs.delete(this.destinationPath(sModuleName, file));
                     } catch (e) {
                         //ignore as this probably means the file doens't exist anyway
                     }
                 });
+
+                // fix incorrect route
+                this.fs.extendJSON(this.destinationPath(sModuleName, "webapp/manifest.json"), { "sap.ui5" : { routing: { routes: [
+                    {
+                      name: "RouteMainView",
+                      pattern: ":?query:",
+                      target: [
+                        "TargetMainView"
+                      ]
+                    }
+                  ]}}});
 
                 this.log(
                     `used ${chalk.blueBright("@sap-ux/fiori-freestyle-writer")} to generate freestyle app skeleton :)`
@@ -229,9 +240,9 @@ module.exports = class extends Generator {
                         _ui5libs = "resources/sap-ui-core.js";
                         break;
                 }
-                await fs.writeFile(
+                this.fs.write(
                     index.html,
-                    (await fs.readFile(index.html)).toString().replace(/src=".*"/g, `src="${_ui5libs}"`)
+                    (this.fs.read(index.html)).toString().replace(/src=".*"/g, `src="${_ui5libs}"`)
                 );
                 this.log(
                     `  ${chalk.blueBright("\u26A0 \uFE0F patched @sap-ux's")} index.html with ${
@@ -243,9 +254,9 @@ module.exports = class extends Generator {
                 // sap.ushell is only available in sapui5
                 // bootstrap only from there, no matter the used framework choice..
                 const flpSandbox = { html: this.destinationPath(sModuleName, "webapp/test/flpSandbox.html") };
-                await fs.writeFile(
+                this.fs.write(
                     flpSandbox.html,
-                    (await fs.readFile(flpSandbox.html))
+                    (this.fs.read(flpSandbox.html))
                         .toString()
                         .replace(/src="(..)\/(test-)?resources/g, (match) => {
                             return match.replace("..", "https://sapui5.hana.ondemand.com");
@@ -262,9 +273,9 @@ module.exports = class extends Generator {
                 const MainViewController = {
                     js: this.destinationPath(sModuleName, "webapp/controller/MainView.controller.js")
                 };
-                await fs.writeFile(
+                this.fs.write(
                     MainViewController.js,
-                    (await fs.readFile(MainViewController.js))
+                    (this.fs.read(MainViewController.js))
                         .toString()
                         .replace(/sap\/ui\/core\/mvc\/Controller/g, "./BaseController")
                 );
