33bd21ebb70a3bee8df860ea71b065c1a37ceb5a
Tobias Queck <tobias.queck@sap.com>
2022-10-12 11:57:15 +0200

    ignore eslint errors in tests

--- generators/qunit/templates/test/testsuite.qunit.js
+++ generators/qunit/templates/test/testsuite.qunit.js
@@ -1,12 +1,15 @@
 window.suite = function () {
     "use strict";
 
+    // eslint-disable-next-line
     var oSuite = new parent.jsUnitTestSuite(),
         sContextPath = location.pathname.substring(0, location.pathname.lastIndexOf("/") + 1);
 
+    // eslint-disable-next-line
     oSuite.addTestPage(sContextPath + "unit/unitTests.qunit.html");
     <% if (typeof(opa5Journeys) !== "undefined") { %>oSuite.addTestPage(sContextPath + "integration/opaTests.qunit.html");
 <% } -%>
 
+    // eslint-disable-next-line
     return oSuite;
 };
