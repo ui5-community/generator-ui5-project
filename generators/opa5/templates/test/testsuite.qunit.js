a0061bc707d51dc2b3d3838bc9e24f90143b5706
Tobias Queck <tobias.queck@sap.com>
2022-10-12 14:38:26 +0200

    eslint ignore for opa test

--- generators/opa5/templates/test/testsuite.qunit.js
+++ generators/opa5/templates/test/testsuite.qunit.js
@@ -1,10 +1,13 @@
 window.suite = function () {
     "use strict";
 
+    // eslint-disable-next-lin
     var oSuite = new parent.jsUnitTestSuite(),
         sContextPath = location.pathname.substring(0, location.pathname.lastIndexOf("/") + 1);
 
+    // eslint-disable-next-lin
     oSuite.addTestPage(sContextPath + "integration/opaTests.qunit.html");
 
+    // eslint-disable-next-lin
     return oSuite;
 };
