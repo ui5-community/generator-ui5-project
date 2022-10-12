3f65e095d2f85b47f0fe2bb774a05c6f2946914f
Tobias Queck <tobias.queck@sap.com>
2022-10-12 15:11:58 +0200

    line is spelled with an e in the end ..

--- generators/opa5/templates/test/testsuite.qunit.js
+++ generators/opa5/templates/test/testsuite.qunit.js
@@ -1,13 +1,13 @@
 window.suite = function () {
     "use strict";
 
-    // eslint-disable-next-lin
+    // eslint-disable-next-line
     var oSuite = new parent.jsUnitTestSuite(),
         sContextPath = location.pathname.substring(0, location.pathname.lastIndexOf("/") + 1);
 
-    // eslint-disable-next-lin
+    // eslint-disable-next-line
     oSuite.addTestPage(sContextPath + "integration/opaTests.qunit.html");
 
-    // eslint-disable-next-lin
+    // eslint-disable-next-line
     return oSuite;
 };
