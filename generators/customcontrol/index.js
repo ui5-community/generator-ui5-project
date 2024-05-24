89eaf0351747d465d6739bb93d176249d0b407d4
nicoschoenteich <nicolai.schoenteich@sap.com>
2024-05-24 17:32:54 +0200

    feat: customcontrol subgenerator

---
+++ generators/customcontrol/index.js
@@ -0,0 +1,30 @@
+import fs from "fs"
+import Generator from "yeoman-generator"
+import prompts from "./prompts.js"
+import { lookForParentUI5ProjectAndPrompt } from "../helpers.js"
+
+export default class extends Generator {
+	static displayName = "Create a new xml view for an existing uimodule."
+
+	async prompting() {
+		await lookForParentUI5ProjectAndPrompt.call(this, prompts)
+	}
+
+	async writing() {
+		this.log(`creating new custom control for ${this.options.config.uimodule}`)
+
+		const webappPath = `${this.options.config.uimodule}/webapp`
+
+		this.fs.copyTpl(
+			this.templatePath("customControl.js"),
+			this.destinationPath(`${webappPath}/control/${this.options.config.controlName}.js`),
+			{
+				superControl: this.options.config.superControl,
+				uimodule: this.options.config.uimodule,
+				controlName: this.options.config.controlName
+			}
+		)
+
+	}
+
+}
---
+++ generators/customcontrol/templates/customControl.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+    "<%= superControl.replace(/\./g, '/') %>"
+], function(SuperControl) {
+    "use strict";
+
+    return SuperControl.extend("<%= uimodule %>.controller.<%= controlName %>",{
+        metadata: {
+            properties: {},
+            aggregations: {},
+        },
+
+        renderer: {},
+
+        <% if (superControl !== "sap.ui.core.Control") { %>
+        onAfterRendering: function() {
+            if(<%=superControl%>.prototype.onAfterRendering) {
+                 <%=superControl%>.prototype.onAfterRendering.apply(this,arguments) //run the super class's method first
+            }
+
+            alert("<%=controlName%> has been rendered!");
+        },
+        <% } %>
+    });
+});
