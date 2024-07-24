3371ba57fb2e51b994bca9b56ce45c5ae9341dc5
D048415 <dominik.heim@sap.com>
2024-07-24 19:08:14 +0200

    fix indentation

--- generators/dependencies.js
+++ generators/dependencies.js
@@ -4,7 +4,7 @@ export default {
 	"@sap-ux/eslint-plugin-fiori-tools": "^0.2",
 	"@sap/approuter": "latest",
 	"@ui5/linter": "latest",
-    "@types/qunit": "2.5.4",
+	"@types/qunit": "2.5.4",
 	"mbt": "^1",
 	"rimraf": "latest",
 	"OpenUI5": "1.120.13",
