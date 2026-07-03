f27a325f85f94bb75432d122582fe0fd30ea3fac
nicoschoenteich <nicolai.schoenteich@sap.com>
2026-07-03 16:12:04 +0200

    bump template deps

--- generators/dependencies.js
+++ generators/dependencies.js
@@ -1,8 +1,8 @@
 // dependencies for generated applications
 // TO-DO: maybe add as peerDependencies for automated security checks via GitHub, then read programmatically from package.json
 export default {
-	"@sap/cds-dk": "^9",
-	"@sap-ux/eslint-plugin-fiori-tools": "^0.2",
+	"@sap/cds-dk": "^10",
+	"@sap-ux/eslint-plugin-fiori-tools": "^10",
 	"@sap/approuter": "latest",
 	"@ui5/linter": "latest",
 	"cds-plugin-ui5": "latest",
@@ -11,5 +11,5 @@ export default {
 	"OpenUI5": "1.148.1",
 	"SAPUI5": "1.148.3",
 	"ui5-middleware-approuter": "^3",
-	"ui5-task-zipper": "^3.2.1"
+	"ui5-task-zipper": "^3"
 }
